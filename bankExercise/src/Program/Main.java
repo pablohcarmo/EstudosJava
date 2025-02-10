7d09baed193a5257a4042936d19f2927bec0723a
Pablo Carmo <66129972+pablohcarmo@users.noreply.github.com>
2025-02-10 01:06:23 -0300

    Correção do exercíco Banco

--- bankExercise/src/Program/Main.java
+++ bankExercise/src/Program/Main.java
@@ -14,15 +14,16 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Account account = new Account();
+		//Account account = new Account();
+		Account account;
 		
 		System.out.print("Enter account number: ");
 		accountNumber = sc.nextInt();
-		account.setAccountNumber(accountNumber);
+		//account.setAccountNumber(accountNumber);
 		
 		System.out.print("Enter account holder: ");
-		accountHolder = sc.next();
-		account.setAccountHolder(accountHolder);
+		accountHolder = sc.nextLine();
+		//account.setAccountHolder(accountHolder);
 		
 		sc.next();
 		System.out.print("Is there an initial deposit (y/n)? ");
@@ -35,17 +36,22 @@ public class Main {
 		if (confirm == 'y' || confirm == 'Y') {
 			System.out.print("Enter initial deposit value: ");
 			balance = sc.nextDouble();
-			account.deposit(balance);
-			System.out.print("Account data:\n" + account.toString());
-		} else if (confirm == 'n' || confirm == 'N'){
+			account = new Account(accountNumber, accountHolder, balance);
+			//account.deposit(balance);
+			//System.out.print("Account data:\n" + account.toString());
+		} else /*if (confirm == 'n' || confirm == 'N')*/{
+			account = new Account(accountNumber, accountHolder);
 			System.out.print("Account data:\n" + account.toString());
 		}
 		System.out.print("\n\nEnter a deposit value: ");
 		balance = sc.nextDouble();
 		account.deposit(balance);
-		System.out.print("Updated account data:\n" + account.toString());
+		System.out.print("Updated account data:\n" + account);
 		
 		System.out.print("\n\nEnter a withdraw value: ");
+		balance = sc.nextDouble();
+		account.withdraw(balance);
+		System.out.print("Updated account data:\n" + account);
 		
 		sc.close();
 	}
--- bankExercise/src/entities/Account.java
+++ bankExercise/src/entities/Account.java
@@ -6,31 +6,42 @@ public class Account {
 	private double accountBalance;
 	private double tax = 5.00;
 	
+	public Account(int accountNumber, String accountHolder, double initialDeposit) {
+		this.accountNumber = accountNumber;
+		this.accountHolder = accountHolder;
+		deposit(initialDeposit);
+	}
+
+	public Account(int accountNumber, String accountHolder) {
+		super();
+		this.accountNumber = accountNumber;
+		this.accountHolder = accountHolder;
+	}
+
 	public int getAccountNumber() {
 		return accountNumber;
 	}
-	public void setAccountNumber(int accountNumber) {
-		this.accountNumber = accountNumber;
-	}
+
 	public String getAccountHolder() {
 		return accountHolder;
 	}
+	
 	public void setAccountHolder(String accountHolder) {
 		this.accountHolder = accountHolder;
 	}
+	
 	public double getAccountBalance() {
 		return accountBalance;
 	}
 	
-	public void deposit(double balance) {
-		this.accountBalance += balance;
+	public void deposit(double amount) {
+		this.accountBalance += amount;
 	}
 	
-	public void withdraw(double balance) {
-		this.accountBalance -= balance + tax;
+	public void withdraw(double amount) {
+		this.accountBalance -= amount + tax;
 	}
-	
-	
+
 	public String toString() {
 		return "Account "
 				+ getAccountNumber()
